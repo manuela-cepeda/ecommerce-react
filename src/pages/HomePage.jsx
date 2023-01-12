bbc7431785e7cf08b968a48cd22421924f06747e
Manuela Cepeda <manuela.cepeda@avanade.com>
2023-01-12 20:50:02 +0100

    cambios api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 
 import { useContext, useEffect } from 'react';
-import {  Outlet, useNavigate } from 'react-router-dom';
+import {  Outlet, useNavigate, useSearchParams } from 'react-router-dom';
 import { AuthContext } from '../components/AuthContext';
 import Chat from '../components/Chat';
 import NavBar from '../components/NavBar';
@@ -9,19 +9,23 @@ function HomePage() {
 
   const {handleLogin } = useContext(AuthContext);
   const navigate = useNavigate() 
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const token = searchParams.get("cookie")
   
   useEffect(() => {
-    const cookie = document.cookie.split('; ').find((row) => row.startsWith('cOokieCoDer='))?.split('=')[1];
+    // const cookie = document.cookie.split('; ').find((row) => row.startsWith('cOokieCoDer='))?.split('=')[1];
+
     const isAuth = async () => {
       const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/sessions/isUserAuth`,{    
-         headers: {"x-access-token": (cookie || localStorage.getItem('session'))}
+         headers: {"x-access-token": (token || localStorage.getItem('session'))}
      }).then(result=> result.json())
-   
       if(response.auth) {
         handleLogin(response.user)
-        cookie && localStorage.setItem('session', cookie);
+        token && localStorage.setItem('session', token);
+        navigate('/')
       }
-      if(!response.auth) navigate('/login')
+       if(!response.auth) navigate('/login')
      }
     isAuth()
    
